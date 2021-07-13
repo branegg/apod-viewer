4dfd0f812f8f4f19e48c326d42c674a144be1e77
Filip Brankiewicz <filip@guider-ai.com>
2021-07-14 00:48:05 +0200

    display random photo // never show the same photo twice

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,5 @@
+import { IPhoto } from '../types/Photo';
+
 const axios = require('axios');
 
 const api = axios.create({
@@ -6,10 +8,23 @@ const api = axios.create({
 
 const baseParams = { api_key: process.env.REACT_APP_NASA_API_KEY };
 
-export async function getPhoto() {
+export async function getPhoto(): Promise<IPhoto> {
   try {
-    const response = await api.get(`/planetary/apod?api_key=${baseParams.api_key}&date=2021-06-06`);
-    return response.data;
+    const response = await api.get(`/planetary/apod?api_key=${baseParams.api_key}&count=1`);
+    const photo = response.data[0];
+    const { date } = photo;
+
+    const seenPhotos: string[] = JSON.parse(localStorage.getItem('seenPhotos') || '[]');
+
+    if (seenPhotos.includes(date)) {
+      return getPhoto();
+    }
+
+    seenPhotos.push(date);
+
+    localStorage.setItem('seenPhotos', JSON.stringify(seenPhotos));
+
+    return photo;
   } catch (error) {
     let res = 'Sorry, something went wrong';
 
@@ -20,7 +35,7 @@ export async function getPhoto() {
         res = "Sorry, there's no photo for today";
       }
     }
-    return res;
+    throw new Error(res);
   }
 }
 
@@ -40,4 +55,10 @@ export async function getPhotos() {
     }
     return res;
   }
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
--- src/components/PhotoPreview/PhotoPreview.tsx
+++ src/components/PhotoPreview/PhotoPreview.tsx
@@ -6,12 +6,20 @@ import saveIcon from './../../assets/images/save.png';
 export default function PhotoPreview(props: { photo: IPhoto }) {
   const { photo } = props;
 
+  const addToFavorites = (photo: IPhoto) => {
+    const currentPhotos = JSON.parse(localStorage.getItem('favoritePhotos') || '[]');
+
+    currentPhotos.push(photo);
+
+    localStorage.setItem('favoritePhotos', JSON.stringify(currentPhotos));
+  };
+
   return (
     <div className="photo-preview">
       <h3 className="photo-preview__title">{photo.title}</h3>
       <div className="photo-preview__wrapper">
         <img className="photo-preview__image" src={photo.url} alt="" />
-        <button className="photo-preview__button">
+        <button className="photo-preview__button" onClick={() => addToFavorites(photo)}>
           <img className="photo-preview__save" src={saveIcon} alt="" />
           Zapisz
         </button>
